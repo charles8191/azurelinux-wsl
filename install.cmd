ba38b4496273b1a72df280970909f597fbe4dfd4
Charles <c4a@monocles.eu>
2024-11-21 16:16:18 -0500

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 fltmc > nul
 if %errorlevel% NEQ 0 (
-    powershell "start-process -verb runas ^"%~f0^""
+    powershell -command "start-process -verb runas '%~f0'"
     exit /b
 )
 wsl --import RHEL C:\RHEL-WSL rhel.tar
